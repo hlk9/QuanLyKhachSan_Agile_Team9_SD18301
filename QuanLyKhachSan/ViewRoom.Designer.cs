fe1051b0dea0ad8f5a42336af3ec1f3ee34eb9ad
Bắc Đặng <bac5120048a4@gmail.com>
2023-07-26 08:29:34 +0700

    Thiết kế layout Xem danh sách phòng

---
+++ QuanLyKhachSan/ViewRoom.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class ViewRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtSearch = new TextBox();
+            btnSearch = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(26, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 26);
+            label1.TabIndex = 0;
+            label1.Text = "Search:";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            txtSearch.Location = new Point(129, 38);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(439, 30);
+            txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.White;
+            btnSearch.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSearch.Location = new Point(593, 37);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(117, 29);
+            btnSearch.TabIndex = 2;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(26, 87);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(684, 351);
+            dataGridView1.TabIndex = 3;
+            // 
+            // ViewRoom
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(722, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearch);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "ViewRoom";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ViewRoom";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ QuanLyKhachSan/ViewRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class ViewRoom : Form
+    {
+        public ViewRoom()
+        {
+            InitializeComponent();
+            //dataGridView1.Rows[0].DefaultCellStyle.BackColor = Color.Aquamarine;
+        }
+    }
+}
